2b13db3a7b7442be24f92ee62a5921ba30dfb28f
Natalia Kowalczyk <melitele@code42day.com>
2017-04-21 23:38:56 -0700

    inside rectangle includes border

--- test/geobounds.js
+++ test/geobounds.js
@@ -3,12 +3,16 @@ var bounds = require('..');
 /* global describe, it */
 
 describe('bounds', function() {
-	it('should fail on empty', function() {
-		bounds([])([1, 1]).should.be.not.ok();
-	});
+  it('should fail on empty', function() {
+    bounds([])([1, 1]).should.be.not.ok();
+  });
 
   it('should succeed if point is in the range', function() {
     bounds([[[-1, 0], [3, 5]]])([1, 1]).should.be.ok();
+  });
+
+  it('should succeed if point is on the border', function() {
+    bounds([[[0, 0], [1, 1]]])([0, 1]).should.be.ok();
   });
 
   it('should succeed if point is in one of the ranges', function() {
@@ -19,9 +23,13 @@ describe('bounds', function() {
     bounds([[[ 1, 1 ], [ 1, 2 ], [ 2, 2 ], [ 2, 1 ]]])([1.5, 1.5]).should.be.ok();
   });
 
+  it('should succeed if point is on the border of the polygon', function() {
+    bounds([[[ 1, 1 ], [ 1, 2 ], [ 2, 2 ], [ 2, 1 ]]])([1, 1]).should.be.ok();
+  });
+
   it('should succeed if point is in one of the shapes', function() {
-      bounds([[[ 1, 1 ], [ 1, 2 ], [ 2, 2 ], [ 2, 1 ]], [[2, 2], [4, 4]]])([3, 3]).should.be.ok();
-    });
+    bounds([[[ 1, 1 ], [ 1, 2 ], [ 2, 2 ], [ 2, 1 ]], [[2, 2], [4, 4]]])([3, 3]).should.be.ok();
+  });
 
   it('should fail point is in none of the ranges', function() {
     bounds([[[-1, 0], [3, 5]], [[100, 20], [115, 80]]])([4, 1]).should.not.be.ok();
